f621e169fbdbbaadba9dd710d53f8a6e53a50a62
Brandon <brandoncolemarmot@gmail.com>
2022-07-06 11:23:33 -0600

    Theme switcher

--- assets/js/modeswitcher.js
+++ assets/js/modeswitcher.js
@@ -1,22 +1,27 @@
-const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-const currentTheme = localStorage.getItem('theme');
+if (window.CSS && CSS.supports("color", "var(--primary)")) {
+  var toggleColorMode = function toggleColorMode(e) {
+    // Switch to Light Mode
+    if (e.currentTarget.classList.contains("light--hidden")) {
+      // Sets the custom html attribute
+      document.documentElement.setAttribute("color-mode", "light"); // Sets the user's preference in local storage
 
-if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
-
-    if (currentTheme === 'dark') {
-        toggleSwitch.checked = true;
+      localStorage.setItem("color-mode", "light");
+      return;
     }
+    /* Switch to Dark Mode
+    Sets the custom html attribute */
+    document.documentElement.setAttribute("color-mode", "dark"); // Sets the user's preference in local storage
+
+    localStorage.setItem("color-mode", "dark");
+  }; // Get the buttons in the DOM
+
+  var toggleColorButtons = document.querySelectorAll(".color-mode__btn"); // Set up event listeners
+
+  toggleColorButtons.forEach(function(btn) {
+    btn.addEventListener("click", toggleColorMode);
+  });
+} else {
+  // If the feature isn't supported, then we hide the toggle buttons
+  var btnContainer = document.querySelector(".color-mode__header");
+  btnContainer.style.display = "none";
 }
-
-function switchTheme(e) {
-    if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
-    }
-    else {        document.documentElement.setAttribute('data-theme', 'light');
-          localStorage.setItem('theme', 'light');
-    }
-}
-
-toggleSwitch.addEventListener('change', switchTheme, false);
